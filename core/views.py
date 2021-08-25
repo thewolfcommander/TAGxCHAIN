a980d4a8d073b412771f9fd9c62284eb9a65c51e
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-08-25 23:15:03 +0530

    Added some dummy pages and configured

--- core/views.py
+++ core/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
+
+
+def about(request):
+    return render(request, 'core/miscellaneous/about.html')
+
+
+def contact(request):
+    return render(request, 'core/miscellaneous/contact.html')
+
+
+def support(request):
+    return render(request, 'core/miscellaneous/support.html')
