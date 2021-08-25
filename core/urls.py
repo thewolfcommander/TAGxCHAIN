71aa5d1b1fd98976bca7af79c4bc5c226cf1b835
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-08-26 00:10:01 +0530

    Configured Economic Calendar Widget On Home page

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,9 @@ app_name = 'core'
 
 urlpatterns = [
     path('', home, name='home'),
+
+    # Miscellaneuous URL patterns
+    path('miscellaneous/about/', about, name='about'),
+    path('miscellaneous/contact/', contact, name='contact'),
+    path('miscellaneous/support/', support, name='support'),
 ]
