4ce91f65ed1d5c6de709af63ef36769711524c96
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-09-01 20:11:50 +0530

    Added Screeners and My Account Page

--- core/urls.py
+++ core/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('miscellaneous/about/', about, name='about'),
     path('miscellaneous/contact/', contact, name='contact'),
     path('miscellaneous/support/', support, name='support'),
+
+    # Analysis Urls
+    path('analysis/screener/crypto/', crypto_screener, name='crypto_screener'),
+    path('analysis/screener/forex/', forex_screener, name='forex_screener'),
+    path('analysis/screener/stocks/', stock_screener, name='stock_screener'),
 ]
--- core/views.py
+++ core/views.py
@@ -23,3 +23,25 @@ def contact(request):
 
 def support(request):
     return render(request, 'core/miscellaneous/support.html')
+
+
+def crypto_screener(request):
+    """
+    View for handling crypto screener
+    """
+    page_title = "Crypto Screener"
+    return render(request, 'core/analysis/crypto_screener.html', {'page_title': page_title})
+
+def forex_screener(request):
+    """
+    View for handling stock screener
+    """
+    page_title = "Forex Screener"
+    return render(request, 'core/analysis/forex_screener.html', {'page_title': page_title})
+
+def stock_screener(request):
+    """
+    View for handling stock screener
+    """
+    page_title = "Stock Screener"
+    return render(request, 'core/analysis/stock_screener.html', {'page_title': page_title})
