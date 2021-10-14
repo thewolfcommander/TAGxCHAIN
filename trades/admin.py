2105694d5409aa9e0d5e05f0bdc129d90e508ffa
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2021-10-14 16:48:16 +0530

    Admin Configured

--- trades/admin.py
+++ trades/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from trades.models import TradingDay, TradingDayTrade
+
 # Register your models here.
+admin.site.register(TradingDay)
+admin.site.register(TradingDayTrade)
